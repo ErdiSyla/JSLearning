3b885b09150583a14d7988f74657a437a63ed8cb
Erdi Syla <erdisyla6@gmail.com>
2025-07-09 12:04:48 +0200

    Done with chapter 5 of the JS course: Variables.

---
+++ Chapter_1-6/Variables/Exercises/excerises.js
@@ -0,0 +1,12 @@
+const myName = 'Erdi Syla';
+
+console.log('My name is: ' + myName);
+
+const cost = 5 + 2 * 3 + 9;
+console.log(`Cost of food: $${cost}`);
+
+const tax = cost * 0.1;
+console.log(`Tax (10%): $${tax}`);
+
+const totalCost = cost + tax;
+console.log(`Total cost: $${totalCost}`);
---
+++ Chapter_1-6/Variables/variables.js
@@ -0,0 +1,23 @@
+let variable1 = 3;
+console.log(variable1);
+
+let calculation = 2 + 2;
+console.log(calculation);
+console.log(calculation + variable1);
+
+let result = calculation  + variable1;
+console.log(result);
+
+let message = 'Hello';
+console.log(message); console.log(';');
+
+variable1 = 5;
+console.log(variable1);
+
+variable1 = variable1 + 1;
+console.log(variable1);
+
+const variable2 = 3;
+var variable3 = 3;
+
+console.log(typeof(variable2));
