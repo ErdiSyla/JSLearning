a9aaa86d616f7e56437f57e9f3887d809907ec7b
Erdi Syla <erdisyla6@gmail.com>
2025-07-10 11:14:19 +0200

    Commit 5: Done with Chapter 6 of the JS course: Booleans.

---
+++ Chapter_1-6/6_Booleans/booleans.js
@@ -0,0 +1,49 @@
+// console.log(3 > 5 - 5);
+
+// console.log('5.00' === 5);
+
+// if (false) {
+//     console.log('hello');
+// }else {
+//     console.log('else');
+// }
+
+// const age = 15;
+
+// if(age >= 16){
+//     console.log('You can drive.');
+//     console.log('Congrats.');
+// }else if(age >= 14) {
+//     console.log('Almost there.');
+// }else{
+//     console.log('You can not drive.');
+// }
+
+// if(5){
+//     console.log(`truthy`);
+// }
+
+// const cartQuantity = 5;
+
+// if(cartQuantity) {
+//     console.log('cart has products');
+// }
+
+// console.log(!0);
+
+// console.log('text' - 5);
+
+// let undefinedVariable;
+// console.log(undefinedVariable);
+
+const result = true ? 'truthy' : 'falsey';
+
+console.log(result);
+
+false && console.log('hello');
+
+const message = 5 && 'hello';
+console.log(message);
+
+const currency = undefined || 'USD';
+console.log(currency);
