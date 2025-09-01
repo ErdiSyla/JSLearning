f6b234c6123a492f846d6f362b124bd9a15e3730
Erdi Syla <erdisyla6@gmail.com>
2025-09-01 11:21:40 +0200

    Commit 26: Removed default tests and added new tests for formatCurrency method.

---
+++ Chapter_13-18/14_Modules/javascript-amazon-project-main/jasmine-standalone-5.1.1/moneyTest.js
@@ -0,0 +1,15 @@
+import { formatCurrency } from '../scripts/util/money.js'
+
+describe('test Suite: formatCurrency', () => {
+    it('converts cents into dollars', () => {
+        expect(formatCurrency(2095)).toEqual('20.95'); 
+    });
+
+    it('works with 0', () => {
+        expect(formatCurrency(0)).toEqual('0.00'); 
+    });
+
+    it('rounds up to nearest cent', () =>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
