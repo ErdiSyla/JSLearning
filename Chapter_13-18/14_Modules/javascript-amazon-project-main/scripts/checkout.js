5460ac6600f22aae5a7db1ab616c59aae773f271
Erdi Syla <erdisyla6@gmail.com>
2025-09-04 15:29:24 +0200

    Commit 38: Learned about async await.

--- Chapter_13-18/14_Modules/javascript-amazon-project-main/scripts/checkout.js
+++ Chapter_13-18/14_Modules/javascript-amazon-project-main/scripts/checkout.js
@@ -1,7 +1,17 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
+
+async function loadPage(){
+    console.log('load page');
+    return 'value 2';
+}
+
+loadPage().then((value) => {
+    console.log('next step');
+    console.log(value);
+})
 
 export function renderCheckOut(){
     renderOrderSummary();
